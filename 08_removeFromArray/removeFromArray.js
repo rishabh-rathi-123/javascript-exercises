2352d705ae8db36427c6957e96bf411c75741f19
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-06-24 22:21:27 -0700

    Completed excercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) 
+{
+    for (let i = 0; i < arr.length; i++)
+    {
+        for (let j = 0; j < args.length; j++)
+        {
+            if (args[j] === arr[i])
+            {
+                arr.splice(i, 1);
+                i--;
+                break;
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
