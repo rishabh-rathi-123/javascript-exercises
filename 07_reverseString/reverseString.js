048a92b2232adf82e066b3724d91d7c157c246f2
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-06-24 18:28:16 -0700

    Completed excercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = "";
+    for (let i = 0; i < str.length; i++)
+    {
+        result += str[str.length - 1 -i];
+    }
 
+    return result;
 };
 
 // Do not edit below this line
