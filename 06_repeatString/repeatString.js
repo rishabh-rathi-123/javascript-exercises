b1b192d53a87cfa28ab82501e8d2be6c9a7b61ab
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-06-24 17:50:24 -0700

    Completed excerice 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num === 0)  return "";
+    for (let i = 1; i < num; i++)
+    {
+        str += str;
+    }
+    return str;
 };
 
 // Do not edit below this line
