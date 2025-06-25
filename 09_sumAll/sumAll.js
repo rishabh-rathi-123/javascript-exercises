b9ef3c470e79215cecb590f6957b2f426bbb0aaf
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-06-24 22:46:20 -0700

    Completed excercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) 
+{
+    if (a < 0 || b < 0) return "ERROR";
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    let c = Math.min(a, b);
+    let d = Math.max(a, b);
+    let sum = 0;
+    for (; c <= d; c++)
+    {
+        sum += c;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
