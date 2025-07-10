ff4e04d4fe865d794598f0a7484471f894a73825
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-07-09 22:13:48 -0700

    Completed excercise 14, fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0)
+        return "OOPS";
+    else if (num == 0)
+        return 0;
 
+    let num1 = 1;
+    let num2 = 1;
+    let result = 1;
+
+    for (let i = 2; i < num; i++)
+    {
+        result = num1 + num2;
+        num1 = num2;
+        num2 = result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
