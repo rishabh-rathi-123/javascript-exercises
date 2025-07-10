5725ba0b7eff787c22ef9ab68500f93fcc32fb18
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-07-09 22:03:50 -0700

    Completed excercise 13, palindrome.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (strIn) {
 
+    let str = "";
+    for (let i = 0; i < strIn.length; i++)
+    {
+        if (strIn[i].match(/[a-zA-Z0-9]/i))
+            str += strIn[i].toLowerCase();
+    }
+
+    if (str.length === 0)
+        return false;
+    else if (str.length === 1)
+        return true;
+
+    let size = str.length / 2;
+
+    for (let i = 0; i < size; i++)
+    {
+        if (str[i] !== str[str.length - i - 1])
+            return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
