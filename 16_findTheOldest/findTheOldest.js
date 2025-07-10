e8018f4e9e0d7c325d671ce60da6f6410cdbc90e
Rishabh Rathi <irishabh.rathi2009@gmail.com>
2025-07-09 22:37:11 -0700

    Completed excercise 16, findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((acc, person) => {
+        if (acc === null)
+            return person;
+        
+        let death1, death2;
 
+        let currentYear = new Date().getFullYear();
+        if (acc.yearOfDeath === undefined)
+            death1 = currentYear - acc.yearOfBirth;
+        else
+            death1 = acc.yearOfDeath - acc.yearOfBirth;
+
+        if (person.yearOfDeath === undefined)
+            death2 = currentYear - person.yearOfBirth;
+        else
+            death2 = person.yearOfDeath - person.yearOfBirth;
+
+        if (death2 > death1)
+            return person;
+        else
+            return acc;
+    }, null);
 };
 
 // Do not edit below this line
